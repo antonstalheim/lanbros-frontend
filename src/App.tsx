c0c716f0eae61f243fda592bf91f50d97be29be5
Anton Pettersson <anton.pettersson@fore.dev>
2021-02-21 11:10:30 +0100

    Adds routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from './theme'
 import Frontpage from './frontpage/Frontpage';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import Gameservers from './frontpage/Gameservers';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <div>
-      <Frontpage></Frontpage>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Frontpage}></Route>
+          <Route exact path="/gameservers" component={Gameservers}></Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   </ThemeProvider>
 );
