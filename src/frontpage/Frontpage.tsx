c1df986000d27dc88c0d5db13bc261a2d781efe8
Anton Pettersson <anton.pettersson@fore.dev>
2021-02-21 11:11:30 +0100

    Adds routing to gameservers from frontpage

--- src/frontpage/Frontpage.tsx
+++ src/frontpage/Frontpage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Grid, Box, makeStyles, createStyles, Button, Typography } from '@material-ui/core';
 import lanbrosTextLogotype from '../assets/lanbros_logo.png';
 
@@ -20,21 +21,27 @@ const useStyles = makeStyles(() =>
 function Frontpage() {
     const classes = useStyles();
 
+    const history = useHistory();
+    const routeToGameservers = () => history.push('/gameservers');
+    const routeToDiscord = () => {
+        window.location.assign('http://discord.lanbros.net');
+    };
+
     return (
         <Box mt={'45vh'}>
             <Grid container>
                 <Grid item xs={12} style={{ textAlign: 'center' }}>
-                    <img height="auto" style={{maxWidth: '50%'}} src={lanbrosTextLogotype} />
+                    <img height="auto" width="100%" style={{maxWidth: '600px'}} src={lanbrosTextLogotype} />
                 </Grid>
                 <Grid item xs={12} style={{ textAlign: 'center', color: 'white' }}>
                     <Typography component="span" variant="body1">Vi spelar spel. Tillsammans.</Typography>
                 </Grid>
                 <Grid item xs={12} style={{ textAlign: 'center' }}>
                     <Box mt={5}>
-                        <Button style={{marginRight: '5px'}} variant="outlined" size="large" className={classes.jumboButton}>
-                            Gå med
+                        <Button onClick={routeToDiscord} style={{marginRight: '5px'}} variant="outlined" size="large" className={classes.jumboButton}>
+                            Discord
                     </Button>
-                        <Button style={{marginLeft: '5px'}} variant="outlined" size="large" className={classes.jumboButton}>
+                        <Button onClick={routeToGameservers} style={{marginLeft: '5px'}} variant="outlined" size="large" className={classes.jumboButton}>
                             Spelservrar
                     </Button>
                     </Box>
