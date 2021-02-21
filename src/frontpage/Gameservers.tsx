6f3bb58bf07f7ae96b8dcd5f9f7fdb4ed7aeeab9
Anton Pettersson <anton.pettersson@fore.dev>
2021-02-21 11:37:50 +0100

    Adds placeholder descriptions to gameservers component

--- src/frontpage/Gameservers.tsx
+++ src/frontpage/Gameservers.tsx
@@ -1,47 +1,46 @@
 import React from 'react';
-import { Grid, Box, makeStyles, createStyles, Button, Typography, Card, CardHeader, CardContent } from '@material-ui/core';
+import { Grid, Box, Card, CardHeader, CardContent, useMediaQuery, Theme } from '@material-ui/core';
 import lanbrosTextLogotype from '../assets/lanbros_logo.png';
 import { useHistory } from 'react-router-dom';
 
-const useStyles = makeStyles(() =>
-    createStyles({
-
-    })
-);
-
-
 function Gameservers() {
-    const classes = useStyles();
+    const isWide = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
 
     const history = useHistory();
     const routeToHome = () => history.push('/');
 
     return (
         <React.Fragment>
-            <Box pt={2} pb={2} pl={10} style={{backgroundColor: 'white'}}>
+            <Box pt={2} pb={2} pl={10} style={{ backgroundColor: 'white' }}>
                 <Grid container>
                     <Grid item xs={4}>
-                        <img onClick={routeToHome} height="auto" width="100%" style={{ maxWidth: '200px', cursor: 'pointer', mixBlendMode: 'difference', display: 'flex' }} src={lanbrosTextLogotype} />
+                        <img onClick={routeToHome} height="auto" width="100%"
+                            style={{ maxWidth: '200px', cursor: 'pointer', mixBlendMode: 'difference', display: 'flex' }}
+                            src={lanbrosTextLogotype} />
                     </Grid>
                 </Grid>
             </Box>
-            <Box mt={5} ml={10} mr={10}>
-                <Box mt={30}></Box>
+            <Box mt={5} ml={isWide ? 40 : 5} mr={isWide ? 40 : 5}>
+                <Box mt={20}></Box>
                 <Grid container spacing={4}>
-                    <Grid item xs={4}>
+                    <Grid item xs={12}>
                         <Card>
-                            <CardHeader title="En testtitel"></CardHeader>
-                            <CardContent>hurr burr</CardContent>
+                            <CardHeader title="Vår spelserverfilosofi"></CardHeader>
+                            <CardContent>Sedan LANBROS grundades 2013 har vi till och från haft ett antal aktiva spelservrar. Minecraft var starten, men sedan testades även Rust, Counter-strike och Garry's Mod.
+                                Vår filosofi är att hänga med i svängarna, och utifrån våra medlemmars önskemål hyr och administrerar vi tillsammans de spelservrar vi vill. Saknar du något spel i listan nedan? Lägg ett förslag i vår #requests-kanal på Discord. 
+                            </CardContent>
                         </Card>
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={12} sm={6}>
                         <Card>
-                            <CardHeader title="En testtitel"></CardHeader>
+                            <CardHeader title="Minecraft"></CardHeader>
+                            <CardContent>Tre servrar finurligt uppsnurrade i ett Kuberneteskluster med hjälp av Docker. Vår egen hemmasnickare lösning, vilket också ger oss tillgång till extra cool statistik som du kan se nedan.</CardContent>
                         </Card>
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={12} sm={6}>
                         <Card>
-                            <CardHeader title="En testtitel"></CardHeader>
+                            <CardHeader title="Valheim"></CardHeader>
+                            <CardContent>Plocka fram din inre viking och bygg dig en stilig liten koja.</CardContent>
                         </Card>
                     </Grid>
                 </Grid>
